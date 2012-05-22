6425aa27b6d7f6605de6098370c59db5c1c43199
fogus <mefogus@gmail.com>
2012-05-22 18:54:17 -0400

    Adding lein project

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject core.contracts "0.0.1"
+  :description ""
+  :dependencies [[org.clojure/clojure "1.5.0-alpha1"]
+                 [org.clojure/core.unify "0.5.2"]]
+  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
+                     [jline "0.9.94"]
+                     [swank-clojure "1.4.0"]]
+  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
+  :source-path "src/main/clojure"
+  :test-path   "src/test/clojure")
