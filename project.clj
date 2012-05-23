d749593530b7a8e3d0b82e2f904b8ed1b59a85a1
fogus <mefogus@gmail.com>
2012-05-22 20:08:24 -0400

    Adding multi testing

--- project.clj
+++ project.clj
@@ -4,7 +4,14 @@
                  [org.clojure/core.unify "0.5.2"]]
   :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
                      [jline "0.9.94"]
-                     [swank-clojure "1.4.0"]]
+                     [swank-clojure "1.4.0"]
+                     [lein-marginalia "0.7.0"]
+                     [lein-multi "1.1.0"]]
+  :multi-deps {:all [[org.clojure/core.unify "0.5.2"]]
+               "1.2"   [[org.clojure/clojure "1.2.0"]]
+               "1.2.1" [[org.clojure/clojure "1.2.1"]]
+               "1.3"   [[org.clojure/clojure "1.3.0"]]
+               "1.4"   [[org.clojure/clojure "1.4.0"]]}
   :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
   :source-path "src/main/clojure"
   :test-path   "src/test/clojure")
