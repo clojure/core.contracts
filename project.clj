b7e931a0c83a2c93b7eb9889f1f4feeea8950dcc
fogus <mefogus@gmail.com>
2012-07-14 09:10:23 -0400

    bump to next verison

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject core.contracts "0.0.1"
+(defproject core.contracts "0.0.2-SNAPSHOT"
   :description "Contracts programming for Clojure."
   :dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]
                  [org.clojure/core.unify "0.5.3"]]
-  :dev-dependencies [[lein-clojars "0.5.0-SNAPSHOT"]
-                     [jline "0.9.94"]
-                     [swank-clojure "1.4.0"]
-                     [lein-marginalia "0.7.0"]
+  :dev-dependencies [[jline "0.9.94"]
+                     [swank-clojure "1.4.2"]
+                     [lein-marginalia "0.7.1"]
                      [lein-multi "1.1.0"]]
   :multi-deps {:all [[org.clojure/core.unify "0.5.3"]]
                "1.2"   [[org.clojure/clojure "1.2.0"]]
