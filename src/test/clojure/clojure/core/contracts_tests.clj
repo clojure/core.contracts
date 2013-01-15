eb4958426b78b2544f2fb96c2fb724529170c097
fogus <mefogus@gmail.com>
2013-01-15 10:06:27 -0500

    Added in-place fn test of contract application

--- src/test/clojure/clojure/core/contracts_tests.clj
+++ src/test/clojure/clojure/core/contracts_tests.clj
@@ -27,5 +27,17 @@
     (is (thrown? AssertionError (bad-doubler 2)))
     (is (thrown? AssertionError (bad-doubler 2 3)))))
 
+(deftest test-full-contract-on-isolated-fn
+  (let [doubler-contract-full-and-isolated-fn
+        (contract doubler
+          "Test"
+          [x] [pos? => (= (* 2 x) %)]
+          [x y] [(pos? x) (pos? y) => (= (* 2 (+ x y)) %)])]
+
+    (is (= 10 ((partial doubler-contract-full-and-isolated-fn #(* 2 (+ %1 %2))) 2 3)))
+    (is (= 10 ((partial doubler-contract-full-and-isolated-fn #(+ %1 %1 %2 %2)) 2 3)))
+    (is (= 10 ((partial doubler-contract-full-and-isolated-fn #(* 2 %)) 5)))
+    (is (thrown? Error ((partial doubler-contract-full-and-isolated-fn #(* 3 (+ %1 %2))) 2 3)))))
+
 (deftest test-regressions
   (defer "regression testing"))
