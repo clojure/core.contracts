8c923016293d9a001c47cf42548adeaf4a97afff
fogus <mefogus@gmail.com>
2013-02-25 16:07:00 -0500

    Adding syntax for constrained records

--- src/main/clojure/clojure/core/contracts/constraints.clj
+++ src/main/clojure/clojure/core/contracts/constraints.clj
@@ -1,7 +1,8 @@
 (ns clojure.core.contracts.constraints
   (:refer-clojure :exclude [== = not=])
   (:use [clojure.core.contracts.impl.utils :only (defcurry-from)])
-  (:require [clojure.set  :as set]))
+  (:require [clojure.set  :as set]
+            clojure.core.contracts.impl.transformers))
 
 ;; # constraint functions and multimethods
 
@@ -73,4 +74,36 @@
   (sqr_ 100)
   (sqr_ 0)
 )
+
+(defmacro defconstrainedfn
+  "Defines a function using the `contract` vector appearing after the arguments.
+
+       (defconstrainedfn sqr
+         [n] [number? (not= 0 n) => pos? number?]
+         (* n n))
+
+   Like the `contract` macro, multiple arity functions can be defined where each argument vector
+   is immediately followed by the relevent arity expectations.  This macro will also detect
+   if a map is in that constraints position and use that instead under the assumption that
+   Clojure's `:pre`/`:post` map is used instead.
+  "
+  [name & body]
+  (let [mdata (if (string? (first body))
+                {:doc (first body)}
+                {})
+        body  (if (:doc mdata)
+                (next body)
+                body)
+        body  (if (vector? (first body))
+                (list body)
+                body)
+        body  (for [[args cnstr & bd] body]
+                (list* args
+                       (if (vector? cnstr)
+                         (second (#'clojure.core.contracts.impl.transformers/build-constraints-description args cnstr (:doc mdata)))
+                         cnstr)
+                       bd))]
+    `(defn ~name
+       ~(str (:doc mdata))
+       ~@body)))
 
