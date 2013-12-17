0722240b349b06941601f71aa3eb5caaab0c8c0c
Tom Faulhaber <git_net@infolace.com>
2013-12-16 18:39:28 -0800

    Autodoc commit for master/0f55199d

--- index-0.0.6.clj
+++ index-0.0.6.clj
@@ -79,7 +79,7 @@
    :var-type "function",
    :line 52,
    :file "src/main/clojure/clojure/core/contracts/constraints.clj"}
-  {:arglists ([l__861__auto__] [l__861__auto__ r__862__auto__]),
+  {:arglists ([l__896__auto__] [l__896__auto__ r__897__auto__]),
    :name "=",
    :namespace "clojure.core.contracts.constraints",
    :source-url
@@ -92,7 +92,7 @@
    :var-type "function",
    :line 64,
    :file "src/main/clojure/clojure/core/contracts/constraints.clj"}
-  {:arglists ([l__861__auto__] [l__861__auto__ r__862__auto__]),
+  {:arglists ([l__896__auto__] [l__896__auto__ r__897__auto__]),
    :name "==",
    :namespace "clojure.core.contracts.constraints",
    :source-url
@@ -159,7 +159,7 @@
    :var-type "function",
    :line 15,
    :file "src/main/clojure/clojure/core/contracts/constraints.clj"}
-  {:arglists ([l__861__auto__] [l__861__auto__ r__862__auto__]),
+  {:arglists ([l__896__auto__] [l__896__auto__ r__897__auto__]),
    :name "not=",
    :namespace "clojure.core.contracts.constraints",
    :source-url
